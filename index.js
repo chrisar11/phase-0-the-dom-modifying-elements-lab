73b58a96c0f3018249c6c1fe8118386174118d12
Christopher Sarmiento <chrisar11@gmail.com>
2022-07-10 16:16:46 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = 'Christopher is the champion';
+document.body.append(newHeader);
+
